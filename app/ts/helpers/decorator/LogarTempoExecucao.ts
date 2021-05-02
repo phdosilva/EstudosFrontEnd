992dd3228688c9f19f9eaf8f92657845cef2a983
Pedro Henrique <phos@cin.ufpe.br>
2021-05-02 12:32:20 -0300

    Medindo o tempo de execução de métodos

--- app/ts/helpers/decorator/LogarTempoExecucao.ts
+++ app/ts/helpers/decorator/LogarTempoExecucao.ts
@@ -1,13 +1,33 @@
-export function logarTempoExecucao() {
+export function logarTempoExecucao(emSegundos: boolean = false) {
   return function (
     target: any,
     propertyKey: string,
     descriptor: PropertyDescriptor
   ) {
     const metodoOriginal = descriptor.value;
+    let unidade = 'ms';
+    let divisor = 1;
+    if (emSegundos) {
+      unidade = 's';
+      divisor = 1000;
+    }
+
     descriptor.value = function (...args: any[]) {
-      const retorno = metodoOriginal.apply(this, args)
-      return retorno;;
+      console.log("------------------");
+      console.log(
+        `parâmetros passados para o métyodo ${propertyKey}: ${JSON.stringify(
+          args
+        )}`
+      );
+      const t1 = performance.now();
+      const retorno = metodoOriginal.apply(this, args);
+      const t2 = performance.now();
+      console.log(
+        `O retorno do método ${propertyKey} é ${JSON.stringify(retorno)}`
+      );
+      console.log(`O método ${propertyKey} demorou ${(t2 - t1)/divisor} ${unidade}`);
+      return retorno;
     };
+    return descriptor;
   };
 }
