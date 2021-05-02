209f09e5db892be39ca7bc9daa3e47e9bafe54ba
Pedro Henrique <phos@cin.ufpe.br>
2021-05-02 13:05:31 -0300

    Consumindo API externa

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -48,7 +48,24 @@ export class NegociacaoController {
   }
 
   importaDados() {
+    function isOk(res: Response) {
+      if (res.ok) {
+        return res;
+      } else {
+        throw new Error(res.statusText);
+      }
+    }
 
+    fetch("http://localhost:8080/dados")
+      .then((res) => isOk(res))
+      .then((res) => res.json())
+      .then((dados: any[]) => {
+        dados
+          .map((dado) => new Negociacao(new Date(), dado.vezes, dado.montante))
+          .forEach((negociacao) => this._negociacoes.adiciona(negociacao));
+        this._negociacoesView.update(this._negociacoes);
+      })
+      .catch((err) => console.log(err.message));
   }
 }
 
