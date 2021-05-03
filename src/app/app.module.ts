09489c3551ae9d9284be317314f4544477a28532
Pedro Henrique <phos@cin.ufpe.br>
2021-05-03 11:58:27 -0300

    httpclient e injecao de dependencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { PhotosModule } from './photos/photos.module';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
