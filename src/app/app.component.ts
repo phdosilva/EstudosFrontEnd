931f6753d07e94b8dadea138fedc542fe9c53e22
Pedro Henrique <phos@cin.ufpe.br>
2021-05-03 15:19:58 -0300

    Modularizando mais os componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Photo } from './photos/photo/photo';
-import { PhotoService } from './photos/photo/photo.service';
-import { PhotosModule } from './photos/photos.module';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  photos: any[] = [];
-
-  constructor(private photoService: PhotoService) {}
-
-  ngOnInit(): void {
-    this.photoService.listFromUser('flavio').subscribe((photos) => {
-      this.photos = photos;
-    });
-  }
-}
+export class AppComponent {}
