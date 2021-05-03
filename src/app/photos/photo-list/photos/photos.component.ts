068383a247dac22c45b5fead488bff7f61242ccb
Pedro Henrique <phos@cin.ufpe.br>
2021-05-03 16:47:18 -0300

    Change detection

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,32 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { PhotoFormComponent } from '../../photo-form/photo-form.component';
 import { Photo } from '../../photo/photo';
 
 @Component({
   selector: 'ap-photos',
   templateUrl: './photos.component.html',
-  styleUrls: ['./photos.component.css']
+  styleUrls: ['./photos.component.css'],
 })
-export class PhotosComponent implements OnInit {
-
+export class PhotosComponent implements OnChanges {
   @Input() photos: Photo[] = [];
   rows: any[] = [];
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.rows = this.groupColumns(this.photos);
-
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.photos) this.rows = this.groupColumns(this.photos);
   }
 
   groupColumns(photos: Photo[]) {
     const newRows = [];
 
-    for(let index = 0; index < photos.length; index += 3) {
+    for (let index = 0; index < photos.length; index += 3) {
       newRows.push(photos.slice(index, index + 3));
     }
     return newRows;
   }
-
 }
