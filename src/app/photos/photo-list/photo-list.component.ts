519bb876f1c1614c88ad20d6c9f3d498e5e778df
Pedro Henrique <phos@cin.ufpe.br>
2021-05-03 16:36:24 -0300

    Componente exclusive para listar fotos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css'],
 })
 export class PhotoListComponent implements OnInit {
-  photos: any[] = [];
+  photos: Photo[] = [];
 
   constructor(
     private photoService: PhotoService,
